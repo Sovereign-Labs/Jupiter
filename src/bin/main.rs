8d296e317b8f6d62160878b3b876908f00ec23d4
Preston Evans <preston.b.evans@gmail.com>
2022-12-09 11:39:25 -0600

    WIP: implement DaApp trait for Celestia

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,105 +1,21 @@
-use std::io::Cursor;
+use std::collections::HashMap;
 
-use hex_literal::hex;
-use prost::Message;
-use sovereign_node::decode_varint;
-use sovereign_node::payment::MsgPayForData;
-use sovereign_node::share_commit::recreate_commitment;
-use sovereign_node::shares::NamespaceGroup;
-use sovereign_node::CelestiaHeaderResponse;
-use sovereign_node::MalleatedTx;
-use sovereign_node::Tx;
-use sovereign_node::TxType;
-// use sovereign_node::MsgPayForBlob;
-use sovereign_node::skip_compact_share_header;
-use sovereign_node::skip_varint;
-use sovereign_node::test_compact_share_parsing;
-// use sovereign_node::MsgPayForData;
-// use sovereign_node::MsgWirePayForData;
+use sovereign_node::{da_app::Celestia, da_service::CelestiaService};
+use sovereign_sdk::da::DaService;
+
+// const ROLLUP_NAMESPACE: [u8; 8] = *b"sov-test";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let rpc_addr = "http://localhost:26659/header/45000";
-    // // http://localhost:26659/namespaced_data/0000000000000001/height/45963
-
-    // let body = reqwest::get(rpc_addr).await?.text().await?;
-
-    // let response: CeljjestiaHeaderResponse = serde_json::from_str(&body)?;
-    // dbg!(response);
-    parse_tx_namespace();
-    let data = "24QbHDZOsRkBiQMKmwIKAggLEgjbhBscNk6xGRj/ASDmnZWcBiogLxiynyPiv+8YkvcArwSevDEKL2a+kk09Omub1ZbBp0syIPZJYgbTRIKPNNj1pbebX6tyXw2A0FB/U/GsX/rn9PAbOiAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEIgAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABKIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUiDjsMRCmPwcFJr79MiZb7kkJ65B5GSbk0yklZkbeFK4VVoUxKVJu7EdyHHyO0Zw/1ixbwdMUdhiIM1jkxSQTdKDa7fWbfgbkBAwowJFgoEy8prS5/4moiFCEgAaZwj+ARIgLxiynyPiv+8YkvcArwSevDEKL2a+kk09Omub1ZbBp0saQIxiJ3NCSIkro/jUWvNNF1PB8DuQBMBCLaW7LHheoEe20RZwxNyrLXVKTe9OyFlM23hiwCFhnE7ajQr9LoFQZwEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-
-    let group = NamespaceGroup::from_b64(&data)?;
-    let blobs: Vec<sovereign_node::shares::Blob> = group.blobs().collect();
-    for blob in blobs {
-        println!("shares[0] = {:?}", &blob.0[0].as_ref());
-        let data: Vec<u8> = blob.data().collect();
-        println!("Data length: {}", data.len());
-        println!("data = {:?}", &data);
-        let hash = recreate_commitment(4, blob).expect("Commitment must be computable");
-        print!("{}", hex::encode(&hash[..]))
+    let mut current_height = 45963;
+    let mut db = HashMap::new();
+    for i in 0..3 {
+        let block = CelestiaService::get_finalized_at(current_height + i).await?;
+        let hash = block.header.hash();
+        db.insert(hash, block);
     }
 
-    // test_compact_share_parsing(&mut data);
-    // skip_compact_share_header(&mut data);
+    let celestia = Celestia { db };
 
-    Ok(())
-}
-
-fn parse_tx_namespace() -> Result<(), Box<dyn std::error::Error>> {
-    let data = "AAAAAAAAAAEBxQIAAA8AwwIKIJn34lyyM0DcNJMnx76Ss326W3Nnq0ytTS9UO48y7UFdEpwCCnwKegoWL3BheW1lbnQuTXNnUGF5Rm9yRGF0YRJgCi9jZWxlc3RpYTFoNm1kem50dTVheGM1OGg5dnY2bnJhM2NmNDZ5Zm4zdzIybmU1YxII24QbHDZOsRkYiQMiIPKYFmkhhoKjg3HM/c5z1yg62hsf9UwRSE5fXf9shiQjEloKUQpGCh8vY29zbW9zLmNyeXB0by5zZWNwMjU2azEuUHViS2V5EiMKIQM/gcidsoTqnBX6AhGABRwiXOcbig/G4xB1UjDtpCwtDxIECgIIARjCAhIFEICb7gIaQEmQk+k+J1g3k5vpJOd/ypI0jcp43+GEaOcqQEGD5iHfYSsJ+qp5YFIBfwigZhLcS20yF+P38DaYmUAxqbZI6SkYAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-
-    let group = NamespaceGroup::from_b64(&data)?;
-    let blobs: Vec<sovereign_node::shares::Blob> = group.blobs().collect();
-    for blob in blobs {
-        let data: Vec<u8> = blob.data().collect();
-        println!("Data length: {}", data.len());
-
-        let mut data = std::io::Cursor::new(data);
-
-        // skip_varint(&mut data).unwrap();
-        let (len, _) = decode_varint(&mut data)
-            .expect("Varint must be valid")
-            .unwrap();
-
-        dbg!("Tx len", len);
-        let backup = data.clone();
-        let tx = match MalleatedTx::decode(data) {
-            Ok(malleated) => {
-                // The hash length must be 32
-                if malleated.original_tx_hash.len() != 32 {
-                    TxType::Other(Tx::decode(backup)?)
-                } else {
-                    TxType::Pfd(malleated)
-                }
-            }
-            Err(_) => TxType::Other(Tx::decode(backup)?),
-        };
-
-        dbg!(&tx);
-
-        let sdk_tx = match tx {
-            TxType::Pfd(malleated) => {
-                let inner = malleated.tx.clone();
-                Tx::decode(inner)?
-            }
-            TxType::Other(tx) => tx,
-        };
-
-        let body = sdk_tx.body.expect("transaction must have body");
-        for msg in body.messages {
-            dbg!(&msg.type_url);
-            if msg.type_url == "/payment.MsgPayForData" {
-                let pfd = MsgPayForData::decode(std::io::Cursor::new(msg.value))?;
-                dbg!(&pfd);
-                if pfd.signer == "celestia1h6mdzntu5axc58h9vv6nra3cf46yfn3w22ne5c" {
-                    let commitment = pfd.message_share_commitment;
-                    dbg!(hex::encode(commitment));
-                }
-            }
-        }
-
-        // assert_eq!(data_from_node, data);
-    }
     Ok(())
 }
--- src/da_app.rs
+++ src/da_app.rs
@@ -1,55 +1,118 @@
-// use sovereign_sdk::da;
+use std::collections::HashMap;
 
-// use crate::{CelestiaHeader, Sha2Hash, H160};
-// use hex_literal::hex;
+use sovereign_sdk::{
+    core::traits::Address,
+    da::{self, TxWithSender},
+    Bytes,
+};
 
-// pub struct Celestia;
+use crate::{
+    da_service::FilteredCelestiaBlock,
+    share_commit::recreate_commitment,
+    shares::{Blob, BlobIterator, BlobRefIterator},
+    BlobWithSender, CelestiaHeader, Sha2Hash, H160,
+};
+use hex_literal::hex;
 
-// impl da::DaApp for Celestia {
-//     type Blockhash = Sha2Hash;
+pub struct Celestia {
+    pub db: HashMap<tendermint::Hash, FilteredCelestiaBlock>,
+}
 
-//     type Address = H160;
+impl TxWithSender<CelestiaAddress> for BlobWithSender {
+    type Data = BlobIterator;
+    fn sender(&self) -> CelestiaAddress {
+        self.sender.clone()
+    }
 
-//     type Header = CelestiaHeader;
+    fn data(&self) -> Self::Data {
+        self.blob.clone().into_iter()
+    }
+}
 
-//     type Transaction;
+#[derive(Debug, PartialEq, Clone)]
+pub struct CelestiaAddress(Vec<u8>);
 
-//     type InclusionProof;
+impl AsRef<[u8]> for CelestiaAddress {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
+    }
+}
+impl Address for CelestiaAddress {}
 
-//     type InclusionMultiProof;
+impl<'a> TryFrom<&'a [u8]> for CelestiaAddress {
+    type Error = ();
 
-//     type CompletenessProof = ();
+    fn try_from(value: &'a [u8]) -> Result<Self, ()> {
+        Ok(Self(value.to_vec()))
+    }
+}
 
-//     type Error;
+impl da::DaApp for Celestia {
+    type Blockhash = tendermint::Hash;
 
-//     const ADDRESS_LENGTH: usize = 20;
+    type Address = CelestiaAddress;
 
-//     const RELATIVE_GENESIS: Self::Blockhash = Sha2Hash(hex!(
-//         "7D99C8487B0914AA6851549CD59440FAFC20697B9029DF7AD07A681A50ACA747"
-//     ));
+    type Header = CelestiaHeader;
 
-//     fn get_relevant_txs(&self, blockhash: &Self::Blockhash) -> Vec<Self::Transaction> {
-//         todo!()
-//     }
+    type BlobTransaction = BlobWithSender;
 
-//     fn get_relevant_txs_with_proof(
-//         &self,
-//         blockhash: &Self::Blockhash,
-//     ) -> (
-//         Vec<Self::Transaction>,
-//         Self::InclusionMultiProof,
-//         Self::CompletenessProof,
-//     ) {
-//         todo!()
-//     }
+    type InclusionProof = ();
 
-//     fn verify_relevant_tx_list(
-//         &self,
-//         blockheader: &Self::Header,
-//         txs: &Vec<Self::Transaction>,
-//         inclusion_proof: &Self::InclusionMultiProof,
-//         completeness_proof: &Self::CompletenessProof,
-//     ) -> Result<(), Self::Error> {
-//         todo!()
-//     }
-// }
+    type InclusionMultiProof = ();
+
+    type CompletenessProof = ();
+
+    type Error = ();
+
+    const ADDRESS_LENGTH: usize = 20;
+
+    const RELATIVE_GENESIS: Self::Blockhash = tendermint::Hash::Sha256(hex!(
+        "7D99C8487B0914AA6851549CD59440FAFC20697B9029DF7AD07A681A50ACA747"
+    ));
+
+    fn get_relevant_txs(&self, blockhash: &Self::Blockhash) -> Vec<Self::BlobTransaction> {
+        let filtered_block = self
+            .db
+            .get(blockhash)
+            .expect("Must only call get txs on extant blocks");
+
+        let mut output = Vec::new();
+        for blob in filtered_block.rollup_data.blobs() {
+            let commitment = recreate_commitment(4, blob.clone()).expect("blob must be valid");
+            let sender = filtered_block
+                .relevant_txs
+                .get(&commitment[..])
+                .expect("blob must be relevant")
+                .signer
+                .clone();
+
+            let blob_tx = BlobWithSender {
+                blob: blob.into(),
+                sender: CelestiaAddress(sender.as_bytes().to_vec()),
+            };
+            output.push(blob_tx)
+        }
+        output
+    }
+
+    fn get_relevant_txs_with_proof(
+        &self,
+        blockhash: &Self::Blockhash,
+    ) -> (
+        Vec<Self::BlobTransaction>,
+        Self::InclusionMultiProof,
+        Self::CompletenessProof,
+    ) {
+        todo!()
+    }
+
+    fn verify_relevant_tx_list(
+        &self,
+        blockheader: &Self::Header,
+        txs: &Vec<Self::BlobTransaction>,
+        inclusion_proof: &Self::InclusionMultiProof,
+        completeness_proof: &Self::CompletenessProof,
+    ) -> Result<(), Self::Error> {
+        todo!()
+    }
+}
